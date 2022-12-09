be8cff9531bc659109a8499aafedc2ab6249dc73
Letif <115136358+letif9994@users.noreply.github.com>
2022-12-09 09:34:43 -0800

    Read me file is added

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Caostone_2
- national parks search engine
+ national parks search engine.
+
+
+ 1. The home page containes total of three pages. 1. Home page 2. National Parks page. 3. Mountains Page. 
+
+ 
+![Alt text](../../Screenshot%202022-12-09%20at%209.29.13%20AM.png)
+
+
--- js/app.js
+++ js/app.js
@@ -119,15 +119,3 @@ function runSearchPark()
 
 }
 
-var video = document.getElementById("myVideo");
-var btn = document.getElementById("myBtn");
-
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
